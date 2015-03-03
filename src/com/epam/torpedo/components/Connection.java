c3b46c8c5f951d3f58ec625ebab49478622c052e
Krisztian Szabo <Krisztian_Gabor_Szabo@epam.com>
2015-03-03 11:47:21 +0100

    Create connection bean

---
+++ src/com/epam/torpedo/components/Connection.java
@@ -0,0 +1,24 @@
+package com.epam.torpedo.components;
+
+public class Connection {
+
+	private String hostName;
+	private int portNumber;
+
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+
+	public int getPortNumber() {
+		return portNumber;
+	}
+
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+
+}
