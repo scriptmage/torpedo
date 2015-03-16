338ae064ca5301715115e4cae8dc1c9bb33d400b
Krisztian Szabo <gdevbox@gmail.com>
2015-03-17 00:26:56 +0100

    Create new test for Coordinate class

---
+++ test/com/epam/torpedo/components/CoordinateTest.java
@@ -0,0 +1,60 @@
+package com.epam.torpedo.components;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CoordinateTest {
+
+	private Coordinate underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new Coordinate(5, 5);
+	}
+
+	@Test
+	public void testHashCodeWithTwoSameCoordinate() {
+		// GIVEN
+		Coordinate otherCoordinate = new Coordinate(5, 5);
+
+		// WHEN
+
+		// THEN
+		Assert.assertEquals(otherCoordinate.hashCode(), underTest.hashCode());
+	}
+
+	@Test
+	public void testHashCodeWithTwoDifferentCoordinate() {
+		// GIVEN
+		Coordinate otherCoordinate = new Coordinate(1, 1);
+
+		// WHEN
+
+		// THEN
+		Assert.assertNotEquals(otherCoordinate.hashCode(), underTest.hashCode());
+	}
+
+	@Test
+	public void testEqualsWithTwoSameCoordinateShouldEquals() {
+		// GIVEN
+		Coordinate otherCoordinate = new Coordinate(5, 5);
+
+		// WHEN
+
+		// THEN
+		Assert.assertEquals(otherCoordinate, underTest);
+	}
+
+	@Test
+	public void testEqualsWithTwoDifferentCoordinateShouldNoEquals() {
+		// GIVEN
+		Coordinate otherCoordinate = new Coordinate(1, 1);
+
+		// WHEN
+
+		// THEN
+		Assert.assertNotEquals(otherCoordinate, underTest);
+	}
+
+}
