430d2b4f14af116f66045eda6bf0e50f2d11235d
Krisztian Szabo <Krisztian_Gabor_Szabo@epam.com>
2015-03-23 10:47:18 +0100

    Begining a new hunter implementation

---
+++ src/com/epam/torpedo/game/hunters/PreciseHunter.java
@@ -0,0 +1,13 @@
+package com.epam.torpedo.game.hunters;
+
+import com.epam.torpedo.Hunter;
+import com.epam.torpedo.components.Coordinate;
+
+public class PreciseHunter extends Hunter {
+
+	@Override
+	public Coordinate nextShot() {
+		return null;
+	}
+
+}
